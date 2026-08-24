d778054cf80f7c29a4134bf57d2797f0fa4c2317
Robert Jhon D. Aracena <159890330+Aracena18@users.noreply.github.com>
2026-08-24 14:59:18 +0800

    simplify app setup to match class topics

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,17 @@ import 'package:flutter/material.dart';
 import 'screens/home/activity_hub_page.dart';
 
 void main() {
-  runApp(const Cce106App());
+  runApp(const MyApp());
 }
 
-class Cce106App extends StatelessWidget {
-  const Cce106App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CCE-106 Activities',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.indigo,
-        scaffoldBackgroundColor: const Color(0xFFF7F8FC),
-      ),
-      home: const ActivityHubPage(),
+      home: ActivityHubPage(),
     );
   }
 }
