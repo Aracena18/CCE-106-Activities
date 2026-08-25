6cae9ca3947862b638a855f433226187215e9e16
Robert Jhon D. Aracena <159890330+Aracena18@users.noreply.github.com>
2026-08-25 12:43:13 +0800

    Add EdgeInsets activity to menu

--- lib/screens/home/activity_hub_page.dart
+++ lib/screens/home/activity_hub_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../activities/activity_01_widget_designing/activity_01_page.dart';
 import '../../activities/activity_02_alignment/activity_02_page.dart';
 import '../../activities/activity_03_login_register/login.dart';
+import '../../activities/activity_04_edgeinsets/activity_04_page.dart';
 
 class ActivityHubPage extends StatelessWidget {
   const ActivityHubPage({super.key});
@@ -51,6 +52,18 @@ class ActivityHubPage extends StatelessWidget {
                 );
               },
               child: const Text('Activity 3 - Login and Register'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Activity04Page(),
+                  ),
+                );
+              },
+              child: const Text('Activity 4 - EdgeInsets'),
             ),
           ],
         ),
