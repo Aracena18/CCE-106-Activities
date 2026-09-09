b647de6a88a6b7cba40e7a1dedfa9a5f6344bd62
Robert Jhon D. Aracena <159890330+Aracena18@users.noreply.github.com>
2026-09-09 13:36:17 +0800

    Add profile image to Task 6 account model

--- lib/activities/activity_06_social_media/model/account.dart
+++ lib/activities/activity_06_social_media/model/account.dart
@@ -1,6 +1,7 @@
 class Account {
   final String name;
   final String email;
+  final String image;
   final String numFollowers;
   final String numPosts;
   final String numFollowing;
@@ -9,6 +10,7 @@ class Account {
   Account({
     required this.name,
     required this.email,
+    required this.image,
     required this.numFollowers,
     required this.numPosts,
     required this.numFollowing,
