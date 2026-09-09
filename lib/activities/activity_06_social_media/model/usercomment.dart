491ce3bc9bb83b3c927bd5107304b6b29e2d6707
Robert Jhon D. Aracena <159890330+Aracena18@users.noreply.github.com>
2026-09-09 13:36:33 +0800

    Add commenter images to Task 6 comments

--- lib/activities/activity_06_social_media/model/usercomment.dart
+++ lib/activities/activity_06_social_media/model/usercomment.dart
@@ -1,10 +1,12 @@
 class UserComment {
   final String commenterName;
+  final String commenterImage;
   final String commentTime;
   final String commentContent;
 
   UserComment({
     required this.commenterName,
+    required this.commenterImage,
     required this.commentTime,
     required this.commentContent,
   });
