a9f1497d1e7e3eb65d6a21260df2e12263d3e5be
Robert Jhon D. Aracena <159890330+Aracena18@users.noreply.github.com>
2026-09-09 12:41:19 +0800

    Add Task 6 comment model

---
+++ lib/activities/activity_06_social_media/model/usercomment.dart
@@ -0,0 +1,11 @@
+class UserComment {
+  final String commenterName;
+  final String commentTime;
+  final String commentContent;
+
+  UserComment({
+    required this.commenterName,
+    required this.commentTime,
+    required this.commentContent,
+  });
+}
