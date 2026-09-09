0061926584369e73a4f6a0fa45cb2a4c66b5eff4
Robert Jhon D. Aracena <159890330+Aracena18@users.noreply.github.com>
2026-09-09 11:47:34 +0800

    Add Task 5 calculator to activity hub

--- lib/screens/home/activity_hub_page.dart
+++ lib/screens/home/activity_hub_page.dart
@@ -4,6 +4,7 @@ import '../../activities/activity_01_widget_designing/icon_page.dart';
 import '../../activities/activity_02_alignment/activity_02_page.dart';
 import '../../activities/activity_03_login_register/login.dart';
 import '../../activities/activity_04_edgeinsets/activity_04_page.dart';
+import '../../activities/activity_05_calculator/calculator.dart';
 
 class ActivityHubPage extends StatelessWidget {
   const ActivityHubPage({super.key});
@@ -64,6 +65,18 @@ class ActivityHubPage extends StatelessWidget {
                 );
               },
               child: const Text('Task 4 - LoginRegister'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Calculator(),
+                  ),
+                );
+              },
+              child: const Text('Task 5 - Calculator'),
             ),
           ],
         ),
